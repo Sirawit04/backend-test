0650ce805490778d98b77fd73ba77a12984c3891
Sirawit04 <sirawit.noomanoch@gmail.com>
2026-07-03 21:21:30 +0700

    Update test_permutations.py

--- permutations/test_permutations.py
+++ permutations/test_permutations.py
@@ -1,19 +1,1 @@
-import unittest
-from permutations import countSmileys
 
-class TestCountSmileys(unittest.TestCase):
-
-    def test_example_1(self):
-        self.assertEqual(countSmileys([':)', ';(', ';}', ':-D']), 2)
-
-    def test_example_2(self):
-        self.assertEqual(countSmileys([';D', ':-(', ':-)', ';~)']), 3)
-
-    def test_example_3(self):
-        self.assertEqual(countSmileys([';]', ':[', ';*', ':$', ';-D']), 1)
-
-    def test_empty_array(self):
-        self.assertEqual(countSmileys([]), 0)
-
-if __name__ == '__main__':
-    unittest.main()
