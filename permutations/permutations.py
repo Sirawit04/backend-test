21b9e8ca726479a6b3ce5b2de792769481a1e047
Sirawit04 <sirawit.noomanoch@gmail.com>
2026-07-03 21:21:45 +0700

    Update permutations.py

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -1,14 +1,14 @@
-char ='abc'
-arr = []
-def permutation(char):
-    if char not in arr:
-        arr.append(char)
-    else:
-        pass
+# char ='abc'
+# arr = []
+# def permutation(char):
+#     if char not in arr:
+#         arr.append(char)
+#     else:
+#         pass
         
         
         
 
-permutation(char)
-print(arr) 
-print(char[1])  
+# permutation(char)
+# print(arr) 
+# print(char[1])  
